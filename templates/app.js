6c45bd2cdc5477a126956b5e9284e943aef5e4ef
Harshit Tyagi <73154589+tyagiharshit3001@users.noreply.github.com>
2022-09-03 11:45:32 +0530

    Add files via upload
    Adding templates

---
+++ templates/app.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+    	$('#logo').addClass('animated fadeInDown');
+    	$("input:text:visible:first").focus();
+	});
+	$('#username').focus(function() {
+		$('label[for="username"]').addClass('selected');
+	});
+	$('#username').blur(function() {
+		$('label[for="username"]').removeClass('selected');
+	});
+	$('#password').focus(function() {
+		$('label[for="password"]').addClass('selected');
+	});
+	$('#password').blur(function() {
+		$('label[for="password"]').removeClass('selected');
+	});
